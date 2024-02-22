54aeffea28690f90e7b2126f146274f994d21f53
IanM084 <149021773+IanM084@users.noreply.github.com>
2024-02-22 04:16:27 +0000

    1st attempted submission - 2/21 11:16

--- assignments/assignment.py
+++ assignments/assignment.py
@@ -1,5 +1,7 @@
 # %%
 import numpy as np
+import random
+from scipy import sparse
 from typing import Any
 
 
@@ -34,7 +36,15 @@ class PrincipalComponentAnalysis:
         self : object
             Returns the instance itself.
         """
-        pass
+        means = []
+        standard_deviations = []
+        for column in range(X.shape[1]):
+            mean = X[:, column].sum() / X.shape[0]
+            means.append(mean)
+            standard_deviations.append(X[:, column].std())
+        self.mean = np.array(means)
+        self.std = np.array(standard_deviations)
+        return self
 
     def transform(self, X: np.ndarray) -> np.ndarray:
         """
@@ -53,7 +63,17 @@ class PrincipalComponentAnalysis:
         X_new : ndarray of shape (n_samples, n_components)
             Transformed values.
         """
-        pass
+        X = X - self.mean
+        X = X / self.std
+
+        covariance_matrix = np.cov(X)
+        eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
+        self.components = eigenvectors
+        eigenvalues[0 : self.n_components]
+        U = eigenvectors[:, 0 : self.n_components]
+
+        X_new = X @ U
+        return X_new
 
 
 # TODO: implement the LDA with numpy
@@ -91,7 +111,51 @@ class LinearDiscriminantAnalysis:
         5. Sort the eigenvectors by decreasing eigenvalues and choose k eigenvectors with the largest eigenvalues to form a d×k dimensional matrix W.
         6. Use this d×k eigenvector matrix to transform the samples onto the new subspace.
         """
-        pass
+
+        means = []
+        standard_deviations = []
+        for column in range(X.shape[1]):
+            mean = X[:, column].sum() / X.shape[0]
+            means.append(mean)
+            standard_deviations.append(X[:, column].std())
+        self.mean = np.array(means)
+        self.std = np.array(standard_deviations)
+
+        data_dict = {}
+        mean_vector = {}
+        categories = np.unique(y)
+        for category in categories:
+            data_dict[category] = X[np.where(y == category), :]
+            mean_vector[category] = data_dict[category].mean(axis=0)
+        mean_vector["Global"] = X.mean(axis=0)
+
+        self.components = categories
+
+        Sw = None
+        for category in categories:
+            if Sw is None:
+                Sw = np.cov(data_dict[category], rowvar=False)
+            else:
+                Sw = Sw + np.cov(data_dict[category], rowvar=False)
+
+        Sb = None
+        for category in categories:
+            centralized_category_mean = mean_vector[category] - mean_vector["Global"]
+            nk = data_dict[category].shape[0]
+            if Sw is None:
+                Sw = nk * np.outer(
+                    (centralized_category_mean, centralized_category_mean.T)
+                )
+            else:
+                Sw = Sw + (
+                    nk
+                    * np.outer((centralized_category_mean, centralized_category_mean.T))
+                )
+
+        self.Sw = Sw
+        self.Sb = Sb
+
+        return self
 
     def transform(self, X: np.ndarray) -> np.ndarray:
         """
@@ -110,7 +174,13 @@ class LinearDiscriminantAnalysis:
         X_new : ndarray of shape (n_samples, n_components)
             Transformed values.
         """
-        pass
+        Sw_inv = np.linalg.inv(self.Sw)
+        eigenvalues, eigenvectors = sparse.linalg.eig(
+            Sw_inv @ self.Sb, k=len(self.components) - 1, which="LM"
+        )
+        X_new = X @ eigenvectors
+
+        return X_new
 
 
 # TODO: Generating adversarial examples for PCA.
@@ -145,4 +215,23 @@ class AdversarialExamples:
             Cluster IDs. y[i] is the cluster ID of the i-th sample.
 
         """
-        pass
+        X = np.zeros([n_samples, n_features])
+        y = np.zeros(n_samples)
+
+        for sample_number in range(int(n_samples / 2)):
+            y[sample_number] = 1
+            X[sample_number, 0:2] = [1 + np.random.normal(), 7 * np.random.normal()]
+        for sample_number in range(int(n_samples / 2), n_samples):
+            y[sample_number] = 2
+            X[sample_number, 0:2] = [
+                10 + np.random.normal(),
+                1 + 8 * np.random.normal(),
+            ]
+
+        X = X - X.mean(axis=0)
+        X = X / X.std(axis=0)
+
+        return X, y
+
+
+# %%
